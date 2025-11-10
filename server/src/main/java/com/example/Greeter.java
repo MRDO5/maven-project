eefddbaaa4002aedc3496f5bd09305926e103e9a
Martin Danielyan <mr.danielyan55@gmail.com>
2025-11-09 20:03:35 -0800

    Add coment for checkstyle 2

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -16,7 +16,6 @@ public class Greeter {
  * @param someon the name of person
  * @return greating string
  */
-  
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
